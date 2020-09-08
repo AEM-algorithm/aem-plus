2624b6b0609e3ab29b6224251378720017630d1a
dee2020 <huzidi@gmail.com>
2020-09-08 17:26:35 +1000

    address detail page

--- src/app/address-book/address-book-routing.module.ts
+++ src/app/address-book/address-book-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
   {
     path: 'add-payee',
     loadChildren: () => import('./add-payee/add-payee.module').then( m => m.AddPayeePageModule)
+  },
+  {
+    path: 'detail',
+    loadChildren: () => import('./detail/detail.module').then( m => m.DetailPageModule)
   }
 ];
 
