f0dc0c8331901de7c23d3144c18d2761efb79a83
dee2020 <huzidi@gmail.com>
2020-09-15 18:13:34 +1000

    wallet page update

--- src/app/wallets/list/list.component.ts
+++ src/app/wallets/list/list.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  wallets = [
+    {
+      walletType: 'BTC',
+      walletAddress: '88070ee21ef642d263a01a21880beef200c3f48b',
+      cryptocurrency: 0.000523,
+      aud: 100
+    },
+    {
+      walletType: 'NEM',
+      walletAddress: '5f089734bdf230d19a954748db1985877e0c13e8',
+      cryptocurrency: 563.278,
+      aud: 200
+    },
+    {
+      walletType: 'ETH',
+      walletAddress: '4ed960dd3722149676b7f37c6d8b81ee732d70c1',
+      cryptocurrency: 0.0927,
+      aud: 600
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {}
