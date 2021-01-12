f8e06685fa8cc416e14796b4699ad2c09af89847
chaofan <chaofan1801@gmail.com>
2021-01-12 11:20:49 +1100

    install bip39 dependency

--- src/app/login/import-account/import-account-routing.module.ts
+++ src/app/login/import-account/import-account-routing.module.ts
@@ -6,16 +6,16 @@ import { ImportAccountPage } from './import-account.page';
 const routes: Routes = [
   {
     path: '',
-    component: ImportAccountPage
+    component: ImportAccountPage,
   },
   {
     path: 'by-mnemonic',
-    loadChildren: () => import('./by-mnemonic/by-mnemonic.module').then( m => m.ByMnemonicPageModule)
+    loadChildren: () => import('./by-mnemonic/by-mnemonic.module').then((m) => m.ByMnemonicPageModule),
   },
   {
     path: 'by-private-key',
-    loadChildren: () => import('./by-private-key/by-private-key.module').then( m => m.ByPrivateKeyPageModule)
-  }
+    loadChildren: () => import('./by-private-key/by-private-key.module').then((m) => m.ByPrivateKeyPageModule),
+  },
 ];
 
 @NgModule({
