4c3e33052567a4fc436e0d371ee69bb1c05b1734
dee2020 <huzidi@gmail.com>
2021-01-14 11:40:40 +1100

    address book detail page style

--- src/app/address-book/detail/detail.page.ts
+++ src/app/address-book/detail/detail.page.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailPage implements OnInit {
 
+  address = [
+    {
+      walletType: 'BTC',
+      walletAddress: '88070ee21ef642d263a01a21880beef200c3f48b'
+    },
+    {
+      walletType: 'NEM',
+      walletAddress: '1edd072aad695cf469832e2d473dca2eec0d5ef9'
+    },
+    {
+      walletType: 'ETH',
+      walletAddress: '7c4a8d09ca3762af61e59520943dc26494f8941b'
+    }
+  ]
+
+
+
   constructor() { }
 
   ngOnInit() {
