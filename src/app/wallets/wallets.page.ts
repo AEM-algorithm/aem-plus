68e4cbb9548d9ab705926430c8bb73e88a269299
chaofan <chaofan1801@gmail.com>
2021-01-18 17:17:51 +1100

    clean up

--- src/app/wallets/wallets.page.ts
+++ src/app/wallets/wallets.page.ts
@@ -1,24 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-wallets',
   templateUrl: './wallets.page.html',
   styleUrls: ['./wallets.page.scss'],
 })
-
 export class WalletsPage implements OnInit {
-  walletSegmentModel: string;
+  constructor() {}
 
-  constructor() {
-
-  }
-
-  ngOnInit() {
-    this.walletSegmentModel = 'myAccount';
-  }
-
-  segmentChanged(ev: any) {
-    console.log('Segment changed', ev);
-  }
-
+  ngOnInit() {}
 }
