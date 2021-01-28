01c1cf861672e1088bd944e54f6db80ea1ee41a7
chaofan <chaofan1801@gmail.com>
2021-01-28 14:21:44 +1100

    refactoring model style & add close action & clean up

--- src/app/wallets/select-wallet-modal/select-wallet-modal.component.ts
+++ src/app/wallets/select-wallet-modal/select-wallet-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
 import { ModalController } from '@ionic/angular';
+
 import { Wallet } from 'src/app/services/models/wallet.model';
 
 @Component({
@@ -11,44 +13,6 @@ import { Wallet } from 'src/app/services/models/wallet.model';
 export class SelectWalletModalComponent implements OnInit {
   @Input() mode: 'send' | 'receive';
   @Input() selectedWallet: Wallet;
-
-  btcTokenList = [
-    {
-      tokenName: 'Token 1',
-      btc: 0.00023,
-      aud: 200.89,
-    },
-    {
-      tokenName: 'Token 2',
-      btc: 0.0023023,
-      aud: 230.89,
-    },
-    {
-      tokenName: 'Token 3',
-      btc: 0.000423,
-      aud: 20.89,
-    },
-    {
-      tokenName: 'Token 4',
-      btc: 5.00023,
-      aud: 21234123340.89,
-    },
-    {
-      tokenName: 'Token 5',
-      btc: 0.03323,
-      aud: 20120.89,
-    },
-    {
-      tokenName: 'Token 6',
-      btc: 0.0073,
-      aud: 20230.89,
-    },
-    {
-      tokenName: 'Token 7',
-      btc: 0.000993,
-      aud: 200.89232,
-    },
-  ];
 
   constructor(private modalCtrl: ModalController, private router: Router) {}
 
