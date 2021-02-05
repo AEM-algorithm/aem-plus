cb0d936bd1756796ec89773f3c30d11a9c9e8c1e
chaofan <chaofan1801@gmail.com>
2021-02-05 13:23:48 +1100

    Outsourcing wallets mock data & create transaction model & mock data

---
+++ src/app/services/dummyData/wallets.data.ts
@@ -0,0 +1,73 @@
+import { Wallet } from '../models/wallet.model';
+
+export const wallets: Wallet[] = [
+  new Wallet(
+    'w1',
+    'u1',
+    'myBTCwallet1',
+    'BTC',
+    'sjdfasfl45asdfass454dfasdfsd',
+    [100.0, 0.0000123],
+    false,
+    [],
+    'fkjhsdfsdfa',
+    ['btc', 'first', 'key', 'more', 'any']
+  ),
+  new Wallet(
+    'w2',
+    'u1',
+    'myXEMwallet1',
+    'NEM',
+    'jknlkasdfjaskdnfaksldfwieeesdf',
+    [100.0, 0.0000123],
+    false,
+    [
+      { name: 'exmToken1', balance: [78.0, 0.0000042] },
+      { name: 'exmToken2', balance: [22.0, 0.0000012] },
+    ],
+    'fkjhsdfsdfa',
+    ['xem', 'first', 'key', 'more', 'any']
+  ),
+  new Wallet(
+    'w3',
+    'u1',
+    'myETHwallet1',
+    'ETH',
+    'dfjasdfjasldkfja34sldkfjlasfdfasdfa',
+    [172.23, 0.0000233],
+    false,
+    [
+      { name: 'ethToken1', balance: [52.0, 0.0000082] },
+      { name: 'ethToken2', balance: [122.0, 0.0000032] },
+    ],
+    'fkjhsdfsdfa',
+    ['eth', ' first', 'key', 'more', 'any']
+  ),
+  new Wallet(
+    'w4',
+    'u1',
+    'myBTCwallet2',
+    'BTC',
+    'fsdfahythdsasdf345erddfgdfsadfht',
+    [423.0, 0.0000023],
+    false,
+    [],
+    'fkjhsdfsdfa',
+    ['btc', 'second', 'key', 'more', 'any']
+  ),
+  new Wallet(
+    'w5',
+    'u1',
+    'myXEMwallet2',
+    'NEM',
+    'fsfgrtutyrrsewr675ewsasdffdhjtutuyeq',
+    [289.0, 0.0000343],
+    false,
+    [
+      { name: 'exm2Token1', balance: [120.0, 0.000029] },
+      { name: 'exm2Token2', balance: [169.0, 0.000042] },
+    ],
+    'fkjhsdfsdfa',
+    ['xem', 'second', 'key', 'more', 'any']
+  ),
+];
---
+++ src/app/services/models/transaction.model.ts
@@ -0,0 +1,29 @@
+export class Transaction {
+  constructor(
+    public time: number,
+    public incoming: boolean,
+    public address: string, // -------------- address of the user's wallet
+    public fee: number,
+    public amount: number,
+    public hash: string, // ------------------  ???? contains what info???
+    public confirmations: number, //  --------  pending or confirmed?????
+    //  back end without the following data prepoties
+    public amountAUD: number,
+    public businessName: string, // ----------  user's business name
+    public receiver: string, //   ------------  receiver's name
+    public recevierAddress: string, //--------- receiver's wallet address
+    public description: string
+  ) {}
+}
+
+// ===== Backend data structure: =====
+// time: number,
+// incoming: boolean,
+// address: string,
+// fee: number,
+// amount: number,
+// hash: string,
+
+//  ========= questions:
+//    we can get transactions of this wallet by address
+//    how to determine which tokens' transaction????
