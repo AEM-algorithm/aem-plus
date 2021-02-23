722a376f97f77fef960b36535aaac8601889713e
chaofan <chaofan1801@gmail.com>
2021-02-23 13:29:36 +1100

    receive page layout

--- src/app/receive/receive.page.ts
+++ src/app/receive/receive.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import * as kjua from 'kjua';
+
+import { Wallet } from '../services/models/wallet.model';
+import { WalletsService } from '../services/wallets/wallets.service';
 
 @Component({
   selector: 'app-receive',
@@ -6,10 +12,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./receive.page.scss'],
 })
 export class ReceivePage implements OnInit {
+  receiveWallet: Wallet;
+  qrCode: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private walletsService: WalletsService) {
+    this.qrCode = { src: '' };
   }
 
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.receiveWallet = this.walletsService.getWallet(params['walletId']);
+    });
+  }
+
+  ionViewWillEnter() {
+    this._encodeQrCode(this.receiveWallet.walletAddress);
+  }
+
+  private _encodeQrCode(infoQR) {
+    this.qrCode = kjua({
+      size: 256,
+      text: infoQR,
+      fill: '#000',
+      quiet: 0,
+      ratio: 2,
+    });
+  }
 }
