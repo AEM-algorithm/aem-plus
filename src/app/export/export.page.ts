bed8765154455c2308d3dcb1c319c67713db7ef9
chaofan <chaofan1801@gmail.com>
2021-02-24 14:42:16 +1100

    export form validations & purchase alter window & style

--- src/app/export/export.page.ts
+++ src/app/export/export.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-export',
@@ -9,18 +10,42 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ExportPage implements OnInit {
   exportForm: FormGroup;
-  constructor() {}
+
+  constructor(private alterCtrl: AlertController) {}
 
   ngOnInit() {
     this.exportForm = new FormGroup({
-      dateFrom: new FormControl(null),
-      dateTo: new FormControl(null),
-      walletsExport: new FormControl(null), // can selecet multiple wallet
-      paymentWallet: new FormControl(null),
+      dateFrom: new FormControl(null, [Validators.required]),
+      dateTo: new FormControl(null, [Validators.required]),
+      walletsExport: new FormControl(null, [Validators.required]), // can selecet multiple wallet
+      paymentWallet: new FormControl(null, [Validators.required]),
     });
   }
 
   onSubmit() {
-    console.log(this.exportForm);
+    // console.log(this.exportForm);
+    // console.log(this.exportForm.get('dateTo').value);
+    this.alterCtrl
+      .create({
+        header: 'Confirm your In-App purchase',
+        message: 'Do you want to unlock export function for two selected wallets?',
+        cssClass: 'purchase-alter',
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+          },
+          {
+            text: 'Buy',
+            role: 'confirm',
+            handler: () => {
+              console.log('confirm the purchase of wallet export function');
+            },
+          },
+        ],
+      })
+      .then((alterEl) => {
+        alterEl.present();
+      });
   }
 }
