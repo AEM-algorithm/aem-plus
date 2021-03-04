57bf6d7ccfafc1eabac1c41058f5ff7ba518194c
chaofan <chaofan1801@gmail.com>
2021-03-04 16:03:28 +1100

    the layout of the transaction detail modal

--- src/app/wallets/sharedComponents/transaction-item/transaction-detail/transaction-detail.component.ts
+++ src/app/wallets/sharedComponents/transaction-item/transaction-detail/transaction-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { HelperFunService } from 'src/app/services/helper/helper-fun.service';
 import { Transaction } from 'src/app/services/models/transaction.model';
 
 @Component({
@@ -10,9 +11,21 @@ import { Transaction } from 'src/app/services/models/transaction.model';
 export class TransactionDetailComponent implements OnInit {
   @Input() selectedTrans: Transaction;
 
-  constructor(private modalCtrl: ModalController) {}
+  date: string;
+  walletType = 'btc'; // TODO: get the wallet's info
 
-  ngOnInit() {}
+  constructor(private modalCtrl: ModalController, private helperService: HelperFunService) {}
+
+  ngOnInit() {
+    this.getDate();
+  }
+
+  // date format:
+  getDate() {
+    // this.date = this.helperService.dateFormat(new Date(this.selectedTrans.time));
+    this.date = new Date(this.selectedTrans.time).toDateString();
+    // console.log(new Date(this.selectedTrans.time).toDateString());
+  }
 
   close() {
     this.modalCtrl.dismiss();
