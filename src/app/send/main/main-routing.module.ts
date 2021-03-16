e5a9e0b03c5de1df2bf13d9b80ce0618b11b3b10
dee2020 <huzidi@gmail.com>
2021-03-16 12:46:11 +1100

    send - main page

--- src/app/send/main/main-routing.module.ts
+++ src/app/send/main/main-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { SendPage } from './send.page';
+import { MainPage } from './main.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: SendPage
-  },
-  {
-    path: 'main',
-    loadChildren: () => import('./main/main.module').then( m => m.MainPageModule)
+    component: MainPage
   }
 ];
 
@@ -18,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class SendPageRoutingModule {}
+export class MainPageRoutingModule {}
--- src/app/send/send.page.ts
+++ src/app/send/send.page.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SendPage implements OnInit {
 
+  addressList = [
+    {
+      name: 'William Shakespeare'
+    },
+    {
+      name: 'Will Smith'
+    },
+    {
+      name: 'Napoleon Bonaparte'
+    },
+    {
+      name: 'George Walker Bush'
+    },
+    {
+      name: 'Albert Einstein'
+    },
+    {
+      name: 'Thomas Alva Edison'
+    },
+    {
+      name: 'Mark Twain'
+    },
+    {
+      name: 'Isaac Newton'
+    },
+    {
+      name: 'Bill Gates'
+    },
+    {
+      name: 'David Beckham'
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
