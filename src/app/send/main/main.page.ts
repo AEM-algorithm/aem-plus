bd4d83c2ded5b41c1ea9808acc28bde433db9112
dee2020 <huzidi@gmail.com>
2021-03-24 13:57:03 +1100

    send-main-address list v1.0

--- src/app/send/main/main.page.ts
+++ src/app/send/main/main.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { SelectWalletModalComponent } from '../../wallets/select-wallet-modal/select-wallet-modal.component';
+import { AddressListModalComponent } from '../address-list-modal/address-list-modal.component'
+import { Wallet } from 'src/app/services/models/wallet.model';
 
 @Component({
   selector: 'app-main',
@@ -10,7 +14,10 @@ export class MainPage implements OnInit {
   selectedType = 'AUD';
   amountType = [];
 
-  constructor() { }
+  constructor(
+    private changeTokenModalController: ModalController,
+    private addressListModalController: ModalController,
+  ) { }
 
   ngOnInit() {
 
@@ -27,6 +34,32 @@ export class MainPage implements OnInit {
 
   onSelectType(e: any) {
     this.selectedType = e.detail.value;
+  }
+
+  changeToken(wallet: Wallet, mode: 'send'){
+    this.changeTokenModalController
+    .create({
+      component: SelectWalletModalComponent,
+      componentProps: {
+        selectedWallet: wallet, // pass the data of cilcked wallet
+        mode: mode, //nav to send
+      },
+      cssClass: 'height-sixty-modal',
+    })
+    .then((modal) => {
+      modal.present();
+    });
+  }
+
+  showAddressList(){
+    this.addressListModalController
+    .create({
+      component: AddressListModalComponent,
+      cssClass: 'height-sixty-modal',
+    })
+    .then((modal) => {
+      modal.present();
+    });
   }
 
 }
