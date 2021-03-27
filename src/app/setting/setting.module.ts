cb5ed3ed8a56c89b88542a3731cf8a41111e0456
dee2020 <huzidi@gmail.com>
2021-03-27 22:44:15 +1100

    setting - select country - add search function

--- src/app/setting/setting.module.ts
+++ src/app/setting/setting.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
+import { IonicSelectableModule } from 'ionic-selectable'
 
 import { SettingPageRoutingModule } from './setting-routing.module';
 
@@ -13,6 +14,7 @@ import { SettingPage } from './setting.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    IonicSelectableModule,
     SettingPageRoutingModule
   ],
   declarations: [SettingPage]
