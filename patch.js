922fc5300f26189b650fee2641622715b8da9ed3
Hiep Huynh Van <hiep.huynh@fdssoft.com>
2021-10-04 11:10:30 +0700

    fix bitcore-explorers error, add script start:init

--- patch.js
+++ patch.js
@@ -17,3 +17,22 @@ fs.readFile(f, 'utf8', function(err, data) {
 		}
 	});
 });
+
+
+const bitcoreLib = 'node_modules/bitcore-explorers/node_modules/bitcore-lib/index.js';
+
+fs.readFile(bitcoreLib, 'utf8', function(err, data) {
+	if (err) {
+		return console.log(err);
+	}
+	const result = data.replace(
+		`throw new Error(message);`,
+		`return null;`
+	);
+	console.log(result)
+	fs.writeFile(bitcoreLib, result, 'utf8', function(err) {
+		if (err) {
+			return console.log(err);
+		}
+	});
+});
