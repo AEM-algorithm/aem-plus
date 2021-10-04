7f17bbcb7a146800298947504528ab1464485475
Hiep Huynh Van <hiep.huynh@fdssoft.com>
2021-10-04 16:17:09 +0700

    [login-page] check login using pin

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuController, NavController } from '@ionic/angular';
+
+import { NavController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
+
 import { TranslateService } from '@ngx-translate/core';
+import { PinProvider } from 'src/app/services/pin/pin.provider';
+import { WalletProvider } from 'src/app/services/wallets/wallet.provider';
+import { AlertProvider } from 'src/app/services/alert/alert.provider';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -12,14 +18,13 @@ export class LoginPage implements OnInit {
   constructor(
     public navCtrl: NavController,
     public translate: TranslateService,
-    private storage: Storage) { }
+    private storage: Storage,
+    private pin: PinProvider,
+    private walletProvider: WalletProvider,
+    private alertProvider: AlertProvider,
+    ) {}
 
   ngOnInit(): void {
-    this.storage.get('mnemonic').then(mnemonic => {
-      if (mnemonic) {
-        this.navCtrl.navigateRoot('/tabnav/wallets');
-      }
-    });
   }
 
 
@@ -35,5 +40,21 @@ export class LoginPage implements OnInit {
    */
   public goToSignupPage() {
     this.navCtrl.navigateForward('/signup');
+  }
+
+  handleCreateAccountClick() {
+    this.navCtrl.pop();
+  }
+
+  async handleLoginClick() {
+    const pin = await this.pin.showEnterPin();
+    if (pin) {
+      const mnemonic = await this.walletProvider.getMnemonic(pin);
+      if (mnemonic) {
+        this.navCtrl.navigateRoot('/tabnav/wallets');
+      } else {
+        this.alertProvider.showIncorrectPassword();
+      }
+    }
   }
 }
