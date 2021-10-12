7dfe10832e7259bf09aeba504247ba199da22309
Hiep Huynh Van <hiep.huynh@fdssoft.com>
2021-10-12 11:05:45 +0700

    [utils-time-helper] getTransactionDate

---
+++ src/utils/TimeHelpers.ts
@@ -0,0 +1,17 @@
+import { Deadline } from 'symbol-sdk';
+import * as moment from 'moment';
+
+export class TimeHelpers {
+  /**
+   * Returns Transaction date String
+   * @param {Deadline} transactionDeadline
+   * @param {number} deadlineInHours based on transaction type
+   * @return {string}
+   */
+  public static getTransactionDate(transactionDeadline: Deadline, deadlineInHours: number, epochAdjustment: number, format: string) {
+    return moment(String(transactionDeadline
+      .toLocalDateTime(epochAdjustment)
+      .minusHours(deadlineInHours)))
+      .format(format);
+  }
+}
