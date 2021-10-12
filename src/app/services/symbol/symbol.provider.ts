74482258bf240cc6b2a7334c1689e4dccd5d0aa1
Hiep Huynh Van <hiep.huynh@fdssoft.com>
2021-10-12 09:30:38 +0700

    [symbol.provider] add function getXYMBalance

--- src/app/services/symbol/symbol.provider.ts
+++ src/app/services/symbol/symbol.provider.ts
@@ -46,11 +46,16 @@ export class SymbolProvider {
     transactionStatusHttp: TransactionStatusHttp;
     mnemonicPassphrase: MnemonicPassPhrase;
 
-    public node: string = 'http://ngl-dual-304.symbolblockchain.io:3000';
+    // NODE MAIN NET
+    // public node: string = 'http://ngl-dual-304.symbolblockchain.io:3000';
+
+    // NODE TEST NET
+    public node: string = 'http://ngl-dual-301.testnet.symboldev.network:3000';
     public isNodeAlive: boolean = false;
 
     //FIXME change mosaic id and generation hash
-    public readonly symbolMosaicId = '6BED913FA20223F8';
+    // public readonly symbolMosaicId = '6BED913FA20223F8'; MAIN NET
+    public readonly symbolMosaicId = '091F837E059AE13C'; // TEST NET
     public readonly epochAdjustment = 1615853185;
     public readonly networkGenerationHash = '57F7DA205008026C776CB6AED843393F04CD458E0AA2D9F1D5F31A402072B2D6';
 
@@ -203,18 +208,25 @@ export class SymbolProvider {
      * @param address address to check balance
      * @return Promise with mosaics information
      */
-    public async getCATBalance(address: Address): Promise<number> {
+    async getXYMBalance(address: Address): Promise<number> {
         try {
-            const accountInfo = await this.accountHttp.getAccountInfo(address).toPromise();
-            let amount = 0;
-            accountInfo.mosaics.forEach(mosaic => {
-                if (mosaic.id.toHex() == this.symbolMosaicId) {
-                    amount = mosaic.amount.compact() / Math.pow(10, 6);
-                }
-            });
-            return amount;
-        } catch (e) {
-            console.log(e);
+            const balances = await this.getBalance(address);
+
+            const balanceByMosaicId = balances.find((item) => item.mosaic.id.toHex() === this.symbolMosaicId);
+
+            if (balanceByMosaicId) {
+                const mosaic: Mosaic = balanceByMosaicId.mosaic;
+                const mosaicInfo: MosaicInfo = balanceByMosaicId.info;
+                const mathPow = Math.pow(
+                  10, mosaicInfo.divisibility
+                );
+                const balance = mosaic.amount.compact() / mathPow;
+                return balance;
+            } else {
+                return 0;
+            }
+        }catch (e) {
+            console.log('symbol.provider', 'getXYMBalance()', 'error:', e);
             return 0;
         }
     }
@@ -244,9 +256,9 @@ public formatLevy(mosaic: MosaicTransferable): Promise<number> {
  * @return Return prepared transaction
  *//*
       public isValidAddress(address: Address): boolean  {
-   
+
       }
-   
+
       /**
        * Prepares xem transaction
        * @param recipientAddress recipientAddress
