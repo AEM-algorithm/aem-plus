1c46f7a4672c7add1a5c212976976a726546e4b5
Dinh-Nguyen <dinh.nguyen@fdssoft.com>
2021-11-01 16:56:48 +0700

    [export tab] use real data

--- src/app/export/export.page.ts
+++ src/app/export/export.page.ts
@@ -8,6 +8,7 @@ import { Wallet } from '../services/models/wallet.model';
 import { WalletsService } from '../services/wallets/wallets.service';
 import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
 import { Router, ActivatedRoute } from '@angular/router';
+import { WalletProvider } from '@app/services/wallets/wallet.provider';
 
 
 @Component({
@@ -17,12 +18,16 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class ExportPage implements OnInit {
   exportForm: FormGroup;
+  isLoading = false;
   isShowWalletType = false;
   isShowWallet = false;
   isShowFrom = false;
   isShowbtn = false;
   coinValue = 'Choose a wallet type';
-  walletValue = 'Choose wallets'
+  coinSelect;
+  walletTypeChoose = false;
+  walletValue = 'Choose wallets';
+  walletAddress;
   type: string; // selected export wallet type
   purchaseFee: number = 12; // unlock export fee: hardcode now
   wallets: Wallet[]; // wallets of the selected type that the user has
@@ -34,8 +39,8 @@ export class ExportPage implements OnInit {
   exportFormData: {
     dateFrom: Date;
     dateTo: Date;
-    walletType: string;
-    walletsExport: Wallet[];
+    // walletType: string;
+    // walletsExport: Wallet[];
     exportFee: number;
   };
 
@@ -44,8 +49,8 @@ export class ExportPage implements OnInit {
     { id: 2, wallet: 'My wallet 2 (BTC)', isSelect: false },
     { id: 3, wallet: 'My wallet 3 (NEM)', isSelect: false },
     { id: 4, wallet: 'My wallet 4 (ETH)', isSelect: false },
-    ];
-  arrayWalletType = ['BTC', 'NEM', 'ETH'];
+  ];
+  arrayWalletType = [];
 
   walletsToExportSelected = false;
 
@@ -53,7 +58,7 @@ export class ExportPage implements OnInit {
   //       set it to true, if user unlock the export function
   //       change the confirm modal info without the purchase fee
   isExportUnlocked = false; // -------  needs to store server-side under this user.
-  datepipe: any;
+  datepipe = [];
   // isExportUnlocked = true; // testing
 
   constructor(
@@ -63,18 +68,48 @@ export class ExportPage implements OnInit {
     private loadingCtrl: LoadingController,
     private router: Router,
     private route: ActivatedRoute,
+    private walletProvider: WalletProvider
   ) { }
 
-  ionViewWillEnter() { }
+  async ionViewWillEnter() {
 
-  ngOnInit() {
-    this.wallets = this.walletsService.getSameTypeWallets('BTC');
+    // this.wallets = await this.wallet.getAllWallets();
+    // this.allBalanceInAud = await this.walletsService.getAllBalanceAud();
+    // this.notificationCounts = await this.notificationService.getAllNotificationCounts();
+    this.arrayWalletType = [];
+    let allWallet = await this.walletProvider.getAllWallets();
+    this.isLoading = true;
+    let json;
+    allWallet.forEach((element, index) => {
+      json =
+      {
+        "walletType": element.walletType,
+        "wallet": [{
+          "id": index,
+          "walletName": element.walletName,
+          "walletAddress": element.walletAddress,
+          "isSelect": false
+        }
+        ],
+      }
+      this.arrayWalletType.push(json);
+    });
+  }
+  async ionViewWillLeave(){
+    this.arrayWalletType = [];
+  }
 
+  async ngOnInit() {
+    // this.wallets = this.walletsService.getWallets();
+    // this.wallets = await this.walletProvider.getAllWallets();
+
+    // this.wallets = this.walletsService.getSameTypeWallets('BTC');
+    this.arrayWalletType = [];
     this.exportForm = new FormGroup({
       dateFrom: new FormControl(null, [Validators.required]),
       dateTo: new FormControl(null, [Validators.required]),
-      walletType: new FormControl('BTC', [Validators.required]),
-      walletsExport: new FormControl(null, [Validators.required]), // can selecet multiple wallet
+      // walletType: new FormControl('BTC', [Validators.required]),
+      // walletsExport: new FormControl(null, [Validators.required]), // can selecet multiple wallet
     });
   }
 
@@ -83,29 +118,27 @@ export class ExportPage implements OnInit {
     //  ---- get the selected type wallets of this users possessed
     this.wallets = this.walletsService.getSameTypeWallets(type);
     //  ---- empty the walletExport every time user select the type
-    this.exportForm.get('walletsExport').setValue(null);
+    // this.exportForm.get('walletsExport').setValue(null);
   }
 
   onSelectExportWallets(e: any) {
     this.walletsToExport = e.detail.value;
     this.walletsToExportSelected = true;
   }
-  onSubmit(){
-   if( this.checkValidate()){
-    this.isShowbtn = true;
-   }
+  onSubmit() {
+    if (this.checkValidate()) {
+      this.isShowbtn = true;
+    }
   }
 
   onSubmit_() {
     const type = this.exportForm.get('walletType').value;
-    console.log
-    console.log('from ' + new Date(this.exportForm.get('dateFrom').value));
-    console.log('to ' + new Date(this.exportForm.get('dateTo').value));
+
     this.exportFormData = {
       dateFrom: new Date(this.exportForm.get('dateFrom').value),
       dateTo: new Date(this.exportForm.get('dateTo').value),
-      walletType: type,
-      walletsExport: this.walletsToExport,
+      // walletType: type,
+      // walletsExport: this.walletsToExport,
       exportFee: this.purchaseFee,
     };
 
@@ -189,39 +222,45 @@ export class ExportPage implements OnInit {
       this.isShowWallet = true
     }
   }
-  chooseWallet(id) {
-    this.arrayWallet.forEach(element => {
-      if(element.id == id){
-        element.isSelect = true;
+  chooseWallet(id, walletType) {
+    this.arrayWalletType.forEach(element => {
+      if (element.walletType == walletType) {
+        element.wallet[0].isSelect = true;
       }
     });
-    let a = this.arrayWallet.filter(wallet => wallet.isSelect == true);
-    this.walletValue = a.map(function(elem){
-      return elem.wallet;
+    let a = this.arrayWalletType.filter(wallet => wallet.walletType == walletType);
+    this.walletValue = a.map(function (elem) {
+      return elem.wallet[0].walletName;
     }).join(",");
     this.onWalletSelect();
     this.onSubmit();
   }
-  chooseWalletDeactive(id) {
-    this.arrayWallet.forEach(element => {
-      if(element.id == id){
-        element.isSelect = false;
+  chooseWalletDeactive(id, walletType) {
+    this.arrayWalletType.forEach(element => {
+      if (element.walletType == walletType) {
+        element.wallet[0].isSelect = false;
       }
     });
-    let a = this.arrayWallet.filter(wallet => wallet.isSelect == true);
-    this.walletValue = a.map(function(elem){
-      return elem.wallet;
+    let a = this.arrayWalletType.filter(wallet => wallet.walletType == walletType);
+    this.walletValue = a.map(function (elem) {
+      return elem.wallet[0].walletName;
     }).join(", ");
     this.onWalletSelect();
     this.onSubmit();
   }
-  chooseCoin(cur) {
+  chooseCoin(cur, add) {
+    this.coinSelect = cur;
+    this.walletValue = 'Choose wallets';
+    this.walletAddress = add;
     switch (cur) {
       case 'BTC':
         this.coinValue = 'bitcoin'
         break;
       case 'NEM':
         this.coinValue = 'nem'
+        break;
+      case 'XYM':
+        this.coinValue = 'xym'
         break;
       case 'ETH':
         this.coinValue = 'ethereum'
@@ -230,44 +269,46 @@ export class ExportPage implements OnInit {
       default:
         break;
     }
+    this.walletTypeChoose = true;
     this.onWalletType();
     this.onSubmit();
   }
-  checkValidate(){
-    if(!this.valuefrom){
+  checkValidate() {
+    if (!this.valuefrom) {
       return false
     }
-    if(!this.valueto){
+    if (!this.valueto) {
       return false
     }
-    if(this.coinValue == 'Choose a wallet type'){
+    if (this.coinValue == 'Choose a wallet type') {
       return false
     }
-    if(this.walletValue == 'Choose wallets'){
+    if (this.walletValue == 'Choose wallets') {
       return false
     }
     return true
   }
-  updateMyDateFrom($event){
+  updateMyDateFrom($event) {
     this.valuefrom = $event;
     this.onSubmit();
   }
-  updateMyDateTo($event){
+  updateMyDateTo($event) {
     this.valueto = $event;
     this.onSubmit();
   }
 
-  onContinue(){
+  onContinue() {
     // this.router.navigateByUrl('/tabnav/export-invoice/confirm-export');
     let json = {
-      'from':this.valuefrom,
-      'to':this.valueto,
-      'wallet_type':this.coinValue,
-      'wallet':this.walletValue  
+      'from': this.valuefrom,
+      'to': this.valueto,
+      'wallet_type': this.coinValue,
+      'wallet': this.walletValue,
+      'wallet_address':this.walletAddress
     }
-    this.router.navigate(['/tabnav', 'export','confirm-export'], { queryParams: json });
+    this.router.navigate(['/tabnav', 'export', 'confirm-export'], { queryParams: json });
   }
-  onHistory(){
+  onHistory() {
     this.router.navigateByUrl('/tabnav/export/export-history');
   }
 }
