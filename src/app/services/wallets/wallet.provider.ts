dedbc75d40eef01af335b5b792e9899c67e29bcf
Tri Phan <tri.phan@fdssoft.com>
2021-11-23 14:19:10 +0700

    Update get Wallet by Id method

--- src/app/services/wallets/wallet.provider.ts
+++ src/app/services/wallets/wallet.provider.ts
@@ -322,7 +322,10 @@ export class WalletProvider {
 
   public async getWalletByWalletId(walletId: string, isCheckOnly: boolean = true): Promise<any> {
     const walletType = walletId.split('_')[0] as Coin;
-    const wallets = await this.getAllWalletsData(isCheckOnly, walletType);
+    let wallets = this.allWallet;
+    if (wallets.length == 0) {
+      wallets = await this.getAllWalletsData(isCheckOnly, walletType);
+    }
     return wallets.find((wallet) => wallet.walletId === walletId);
   }
 
