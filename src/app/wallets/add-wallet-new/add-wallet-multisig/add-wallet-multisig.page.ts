222a241ca66f96df213d6b828eb274885beffa7e
Dinh-Nguyen <dinh.nguyen@fdssoft.com>
2021-11-24 14:28:57 +0700

    wallet mutisig

--- src/app/wallets/add-wallet-new/add-wallet-multisig/add-wallet-multisig.page.ts
+++ src/app/wallets/add-wallet-new/add-wallet-multisig/add-wallet-multisig.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Storage } from "@ionic/storage";
 import { Crypto } from 'symbol-sdk';
+import { SUPPORTED_COINS, CoinInfo } from '@app/constants/constants';
 
 @Component({
   selector: 'app-add-wallet-multisig',
@@ -12,23 +13,24 @@ export class AddWalletMultisigPage implements OnInit {
   showSelect = false;
   showCoin = false;
   coin: any;
+  isShowBtn = false;
   error = false;
-  messageError : any;
-  credentials = {
-    username: '',
-    password: '',
-
-  };
+  pk;
+  supportedCoins: any[];
+  messageError: any;
+  isCustomeName = false;
+  isCurrencyType = false;
+  isPrivateKey = false;
+  isCustomName;
   constructor(
     private router: Router,
     private storage: Storage,) { }
 
   ngOnInit() {
-    this.credentials.password = this.generateHexString();
+    this.supportedCoins = Object.values(SUPPORTED_COINS);
   }
 
   selectCoin() {
-
     if (!this.showSelect) {
       this.showSelect = true;
     }
@@ -36,53 +38,49 @@ export class AddWalletMultisigPage implements OnInit {
       this.showSelect = false;
     }
   }
+  onCustomName($event) {
+    this.isCustomName = $event.target.value;
+    if (this.isCustomName) {
+      this.isCustomeName = true;
+      
+    }
+    else{
+      this.isCustomeName = false;
+    }
+
+    this.checkRequired();
+  }
   chooseCoin(coinSelect) {
     this.showCoin = true;
-    switch (coinSelect) {
-      case 'btc':
-        coinSelect = 'Bitcoin (BTC)';
-        break;
-      case 'xem':
-        coinSelect = 'NEM (XEM)'
-        break;
-      case 'eth':
-        coinSelect = 'Ethereum (ETH)'
-        break;
-
-      default:
-        break;
-    }
-    this.coin = coinSelect;
+    this.coin = coinSelect.name;
     this.showSelect = false;
-
+    this.isCurrencyType = true;
+    this.checkRequired();
+  }
+  onPrivateKey($event) {
+    this.pk = $event.target.value;
+    if (this.pk) {
+      this.isPrivateKey = true;
+    }
+    else{
+      this.isPrivateKey = false;
+    }
+    this.checkRequired();
   }
 
   continue() {
-    if(this.checkRequired()){
-      this.error = true;
-    }
-    else{
-      this.storage.remove('address-signer');
-      this.error = false;
-      this.router.navigateByUrl('/tabnav/wallets/add-signer/?a');
-    }
+    this.storage.remove('address-signer');
+    this.error = false;
+    this.router.navigateByUrl('/tabnav/wallets/add-signer/?a');
   }
   checkRequired() {
-    if(!this.credentials.username){
-      this.messageError = 'Please input custom name';
-      return true
-    }
-    else if(!this.coin){
-      this.messageError = 'Please choose currency type';
-      return true
-    }
-    else if(!this.credentials.password){
-      this.messageError = 'Please input private key';
-      return true
+    if (this.isCustomeName && this.isCurrencyType && this.isPrivateKey) {
+      this.isShowBtn = true;
     }
     else{
-      return false
+      this.isShowBtn = false;
     }
+
   }
   generateHexString() {
     const randomString = Crypto.randomBytes(32).toString('hex');
