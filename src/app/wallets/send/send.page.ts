101de89bf286931a716614146d7903ce658a8fef
Hiep Huynh Van <hiep.huynh@fdssoft.com>
2021-11-26 09:29:30 +0700

    [symbol] update selectedMosaic

--- src/app/wallets/send/send.page.ts
+++ src/app/wallets/send/send.page.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Address as SymbolAddress, Mosaic as SymbolMosaic, UInt64 as SymbolUInt64 } from 'symbol-sdk';
 
-import { ModalController } from '@ionic/angular';
+import { ModalController, Platform } from '@ionic/angular';
 
 import { Wallet } from 'src/app/services/models/wallet.model';
 import { Token } from 'src/app/services/models/token.model';
 import { Transaction } from 'src/app/services/models/transaction.model';
 import { WalletsService } from 'src/app/services/wallets/wallets.service';
 import { WalletProvider } from 'src/app/services/wallets/wallet.provider';
+import { SymbolProvider } from '@app/services/symbol/symbol.provider';
 
 import { ConfirmTransactionModalComponent } from './confirm-transaction-modal/confirm-transaction-modal.component';
 import { SelectAddressModalComponent } from './select-address-modal/select-address-modal.component';
 
 import { WALLET_ICON } from 'src/app/constants/constants';
+
+const defaultFeesConfig = {
+  free: 0,
+  slow: 1,
+  normal: 1.2,
+  fast: 2,
+};
 
 @Component({
   selector: 'app-send',
@@ -25,16 +34,18 @@ export class SendPage implements OnInit {
   selectedWallet: Wallet;
   selectedToken: Token;
 
-  cryptoBanlance: number;
-  audBanlance: number;
+  cryptoBalance: number;
+  audBalance: number;
+  selectedMosaic: any;
 
   transformedWalletData: {};
 
-  selectedType = 'AUD';
+  selectedType;
   //  --- form & form inputs:
   sendForm: FormGroup;
-  amountAud: number;
+  amountCurrency: number;
   amountCrypto: number;
+  amount: number;
   receiverName: string;
 
   // ---- form error handling:
@@ -47,12 +58,12 @@ export class SendPage implements OnInit {
   businessName: string;
 
   // --- fee adjustment:
-  suggestedFeeAud: number = 0.0;
-  maxFeeAud: number;
-  minFeeAud: number;
+  suggestedFeeCurrency: number = 0.0;
+  maxFeeCurrency: number;
+  minFeeCurrency: number;
   isTooLow = false;
   isTooHigh = false;
-  selectedFeeAud: number;
+  selectedFeeCurrency: number;
   selectedFeeCrypto: number;
 
   walletIcon = WALLET_ICON;
@@ -62,6 +73,9 @@ export class SendPage implements OnInit {
     private route: ActivatedRoute,
     private walletsService: WalletsService,
     private walletProvider: WalletProvider,
+    private platform: Platform,
+    private symbol: SymbolProvider,
+    private router: Router,
   ) {
     this.selectedWallet = new Wallet(
       '',
@@ -83,30 +97,31 @@ export class SendPage implements OnInit {
       receiverAddress: new FormControl(null, Validators.required),
       amountType: new FormControl(this.selectedType, Validators.required),
       amount: new FormControl(null, Validators.required),
-      fee: new FormControl(this.suggestedFeeAud, Validators.required),
+      fee: new FormControl(this.suggestedFeeCurrency, Validators.required),
       description: new FormControl(null), // optional
     });
   }
 
   ngOnInit() {
     this.route.paramMap.subscribe(async (params) => {
-      console.log('hvh', 'send.page', 'ngOnInit()', ' params:', params);
       const walletId = params.get('walletId');
-      this.selectedWallet = await this.walletProvider.getWalletByWalletId(walletId);
+      const state = this.router.getCurrentNavigation().extras.state;
+      this.selectedMosaic = state?.selectMosaic;
 
-      console.log('hvh', 'send.page', 'ngOnInit()', ' selectedWallet:', this.selectedWallet);
+      this.selectedWallet = await this.walletProvider.getWalletByWalletId(walletId, false);
+
       // this.selectedWallet = this.walletsService.getWallet(params.get('walletId'));
 
-      console.log('selected wallet:', this.selectedWallet);
-      this.cryptoBanlance = this.selectedWallet.walletBalance[1];
-      this.audBanlance = this.selectedWallet.walletBalance[0];
+      this.cryptoBalance = this.selectedWallet.walletBalance[1];
+      this.audBalance = this.selectedWallet.walletBalance[0];
       this.transformedWalletData = this.selectedWallet;
+      this.selectedType = this.selectedWallet.currency;
 
       if (params.has('tokenId')) {
         this.isTokenSelected = true;
         this.selectedToken = this.walletsService.getToken(this.selectedWallet, params.get('tokenId'));
-        this.cryptoBanlance = this.selectedToken.balance[1];
-        this.audBanlance = this.selectedToken.balance[0];
+        this.cryptoBalance = this.selectedToken.balance[1];
+        this.audBalance = this.selectedToken.balance[0];
       }
     });
 
@@ -119,12 +134,13 @@ export class SendPage implements OnInit {
 
   onSelectType(e: any) {
     this.selectedType = e.detail.value;
+    this.onEnterAmount({});
   }
 
   private checkAmountValidation(enteredAmount: number, maxAmount: number) {
     if (enteredAmount > maxAmount) {
       this.isAmountValid = false;
-      this.amountErr = `Avalibal balance is not larger than ${maxAmount}`;
+      this.amountErr = `Available balance is not larger than ${maxAmount}`;
       return;
     }
     this.isAmountValid = true;
@@ -132,52 +148,101 @@ export class SendPage implements OnInit {
 
   onEnterAmount(e: any) {
     // --- get the amount based on selected type:
-    const enteredAmount = e.target.value;
+    const enteredAmount = e?.target?.value;
+    if (enteredAmount) {
+      this.amount = enteredAmount;
+    }
+    if (this.amount === null || this.amount === undefined || isNaN(this.amount)) {
+      return;
+    }
 
-    if (this.selectedType === 'AUD') {
-      // if (enteredAmount > this.audBanlance) {
+    if (this.selectedType === this.selectedWallet.currency) {
+      // if (enteredAmount > this.audBalance) {
       //   this.isAmountValid = false;
-      //   this.amountErr = `Avalibal balance is not larger than ${this.audBanlance}`;
+      //   this.amountErr = `Avalibal balance is not larger than ${this.audBalance}`;
       //   return;
       // }
-      this.checkAmountValidation(enteredAmount, this.audBanlance);
-      this.amountAud = enteredAmount;
-      this.amountCrypto = enteredAmount / 5000; // mock the calculation
+      this.checkAmountValidation(this.amount, this.audBalance);
+      this.amountCurrency = this.amount;
+      this.amountCrypto = this.amount / this.selectedWallet.exchangeRate; // mock the calculation
     } else {
-      // if (enteredAmount > this.cryptoBanlance) {
+      // if (enteredAmount > this.cryptoBalance) {
       //   this.isAmountValid = false;
-      //   this.amountErr = `Avalibal balance is not larger than ${this.cryptoBanlance}`;
+      //   this.amountErr = `Avalibal balance is not larger than ${this.cryptoBalance}`;
       //   return;
       // }
-      this.checkAmountValidation(enteredAmount, this.cryptoBanlance);
+      this.checkAmountValidation(this.amount, this.cryptoBalance);
       // this.isAmountValid = true;
-      this.amountCrypto = enteredAmount;
-      this.amountAud = enteredAmount * 5000; // mock the calculation
+      this.amountCrypto = this.amount;
+      this.amountCurrency = this.amount * this.selectedWallet.exchangeRate;
     }
     //  --- mock the tax calculation:
-    this.tax = (this.amountAud * 0.1) / (1 + 0.1);
+    this.tax = (this.amountCurrency * 0.1) / (1 + 0.1); // TODO
     // ----- mock calculate the fee & set the fee selection range:
-    this.suggestedFeeAud = +(this.amountAud * 0.05).toFixed(2);
-    this.maxFeeAud = +(this.suggestedFeeAud * 2).toFixed(2);
-    this.minFeeAud = +(this.suggestedFeeAud * 0.01).toFixed(2);
+    this.suggestedFeeCurrency = +(this.amountCurrency * 0.05).toFixed(2); // TODO
+    this.maxFeeCurrency = +(this.suggestedFeeCurrency * 2).toFixed(2); // TODO
+    this.minFeeCurrency = +(this.suggestedFeeCurrency * 0.01).toFixed(2); // TODO
+  }
+
+  // private prepareTransaction(recipient: SymbolAddress) {
+  //   const transferTransaction = this.symbol.prepareMosaicTransaction(recipient, [
+  //     new SymbolMosaic(
+  //       this.selectedMosaic.mosaic.id,
+  //       SymbolUInt64.fromUint(this.amount * Math.pow(10, this.selectedMosaic.info.divisibility))
+  //     )
+  //   ], this.sendForm.value.description);
+  //   const fee = transferTransaction.maxFee.compact();
+  //   console.log(fee);
+  //   return transferTransaction;
+  // }
+
+  async updateMaxFee() {
+    setTimeout(async () => {
+      const defaultMaxFees = await Promise.all([
+        ...Object.keys({...defaultFeesConfig, current: defaultFeesConfig.normal})
+          .map(async key =>
+            [
+              key,
+              this.resolveAmount(this.prepareTransaction(defaultFeesConfig[key]), 6)
+            ]
+          )
+      ]);
+
+      console.log(defaultMaxFees);
+    });
+  }
+
+  private prepareTransaction = (fee?) => {
+    const mosaic = this.selectedMosaic.mosaic;
+    mosaic.amount = this.amountCrypto * Math.pow(10, mosaic.divisibility);
+
+    return {
+      recipientAddress: this.sendForm.value.receiverAddress,
+      mosaics: [mosaic],
+      message: this.sendForm.value.description,
+      fee: fee || this.selectedFeeCrypto,
+    };
+  }
+
+  resolveAmount = (rawAmount, divisibility) => {
+    return rawAmount / Math.pow(10, divisibility);
   }
 
   onSelectFee(e: any) {
-    console.log('fee:', e.target.value);
     const selectedVal = e.target.value;
-    this.selectedFeeAud = e.target.value;
+    this.selectedFeeCurrency = e.target.value;
 
     // show the warning when reach a certain point
-    if (selectedVal < this.suggestedFeeAud * 0.02) {
+    if (selectedVal < this.suggestedFeeCurrency * 0.02) {
       this.isTooLow = true;
-    } else if (selectedVal > this.suggestedFeeAud * 1.7) {
+    } else if (selectedVal > this.suggestedFeeCurrency * 1.7) {
       this.isTooHigh = true;
     } else {
       this.isTooLow = false;
       this.isTooHigh = false;
     }
-    this.selectedFeeAud = e.target.value;
-    this.selectedFeeCrypto = +(this.selectedFeeAud * 0.02).toFixed(8); // mock the convertion
+    this.selectedFeeCurrency = e.target.value;
+    this.selectedFeeCrypto = +(this.selectedFeeCurrency * 0.02).toFixed(8); // mock the convertion
   }
 
   showAddressList() {
@@ -207,13 +272,16 @@ export class SendPage implements OnInit {
   onEnterAddress(e: any) {
     // Validate the entered address
     const enteredAddress = e.target.value;
-
     if (enteredAddress.length > 12) {
     }
   }
 
   onEditFee() {
     console.log('editing fee...');
+  }
+
+  onDescriptionChange(e) {
+    this.updateMaxFee();
   }
 
   onSend() {
@@ -227,11 +295,11 @@ export class SendPage implements OnInit {
       incoming: false,
       address: this.selectedWallet.walletAddress,
       feeCrypto: this.selectedFeeCrypto,
-      feeAud: this.selectedFeeAud,
+      feeAud: this.selectedFeeCurrency,
       amount: this.amountCrypto,
       hash: 'jsdfkljasdfasdfasdfasdfarfdadsfdf', //hard code
       confirmations: 9, //hard code
-      amountAUD: this.amountAud,
+      amountAUD: this.amountCurrency,
       businessName: this.businessName,
       receiver: this.receiverName,
       receiverAddress: this.sendForm.value.receiverAddress,
