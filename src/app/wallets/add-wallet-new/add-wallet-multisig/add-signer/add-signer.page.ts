e808dc8cdcc44dfc73f563f2abeafc6de5222d17
Hiep Huynh Van <hiep.huynh@fdssoft.com>
2021-12-23 09:37:09 +0700

    add verify pincode

--- src/app/wallets/add-wallet-new/add-wallet-multisig/add-signer/add-signer.page.ts
+++ src/app/wallets/add-wallet-new/add-wallet-multisig/add-signer/add-signer.page.ts
@@ -2,12 +2,18 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
 import { Subscription } from 'rxjs';
+import { ModalController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 
 import { MemoryProvider } from '@app/services/memory/memory.provider';
-
 import { WalletProvider } from 'src/app/services/wallets/wallet.provider';
 import { LoadingProvider } from '@app/services/loading/loading.provider';
+import { ToastProvider } from '@app/services/toast/toast.provider';
+
+import { PinModalComponent } from 'src/app/pin-modal/pin-modal.component';
+
 import { Coin } from '@app/enums/enums';
+
 @Component({
   selector: 'app-add-signer',
   templateUrl: './add-signer.page.html',
@@ -32,6 +38,9 @@ export class AddSignerPage implements OnInit, OnDestroy {
     private memory: MemoryProvider,
     private wallet: WalletProvider,
     private loading: LoadingProvider,
+    private modal: ModalController,
+    private translate: TranslateService,
+    private toast: ToastProvider,
   ) { }
 
   async ngOnInit() {
@@ -75,18 +84,22 @@ export class AddSignerPage implements OnInit, OnDestroy {
     this.router.navigateByUrl('/tabnav/wallets/add-wallet-new/add-wallet-multisig/add-signer/add-consignator');
   }
 
-  save() {
-    this.loading.presentLoading();
+  async save() {
+    const pin = await this.verifyPinCode();
+    if (!pin) {
+      return;
+    }
+    await this.loading.presentLoading();
     try {
-      this.wallet.generateWalletFromPrivateKey(this.multisigWalletPrivateKey, "pin", this.selectedCoin, this.multisigWalletName, true);
+      await this.wallet.generateWalletFromPrivateKey(this.multisigWalletPrivateKey, pin, this.selectedCoin, this.multisigWalletName, true);
       // TODO: announce create multisig account transaction
-      const result = this.annountMultisigAccountTransaction();
-
+      const result = await this.annountMultisigAccountTransaction();
+      console.log('annountMultisigAccountTransaction', result);
     } catch (error) {
       console.log(error);
     }
-    this.loading.dismissLoading();
-    this.router.navigateByUrl('/tabnav/wallets');
+    await this.loading.dismissLoading();
+    // this.router.navigateByUrl('/tabnav/wallets');
   }
 
   private async annountMultisigAccountTransaction() {
@@ -100,5 +113,27 @@ export class AddSignerPage implements OnInit, OnDestroy {
       default:
         break;
     }
+  }
+
+  private async verifyPinCode(): Promise<string> {
+    const res = await this.translate.get(['CONFIRM_SECURITY', 'ALERT_PROVIDED_PIN_INVALID'], {}).toPromise();
+    const pinModal = await this.modal.create({
+      component: PinModalComponent,
+      cssClass: 'pinModal',
+      componentProps: {
+        title: res['CONFIRM_SECURITY']
+      }
+    });
+    await pinModal.present();
+    const pinData = await pinModal.onDidDismiss();
+    if (pinData.data.pin) {
+      const isValidPin = await this.wallet.isValidPin(pinData.data.pin);
+      if (isValidPin) {
+        return pinData.data.pin;
+      } else {
+        this.toast.showMessageError(res['ALERT_PROVIDED_PIN_INVALID']);
+      }
+    }
+    return null;
   }
 }
