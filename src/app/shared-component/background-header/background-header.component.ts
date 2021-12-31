7f5b32334eb9736a4e7ef6752c60cb7bc1b7e33e
Long Pham <long.pham@fdssoft.com>
2021-12-31 12:42:07 +0700

    ui header/back-buttion left align edit-wallet/download buttion

--- src/app/shared-component/background-header/background-header.component.ts
+++ src/app/shared-component/background-header/background-header.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import { Platform } from "@ionic/angular";
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
-  selector: "app-background-header",
-  templateUrl: "./background-header.component.html",
-  styleUrls: ["./background-header.component.scss"],
+  selector: 'app-background-header',
+  templateUrl: './background-header.component.html',
+  styleUrls: ['./background-header.component.scss'],
 })
 export class BackgroundHeaderComponent implements OnInit {
   boxTopLeft: string;
@@ -12,20 +12,23 @@ export class BackgroundHeaderComponent implements OnInit {
   boxMidRight: string;
   boxBottomRight: string;
   //
+  platform;
   constructor(platform: Platform) {
-    platform.ready().then(() => {
-      let demension = platform.height() * 0.2;
+    this.platform = platform;
+  }
+
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      let demension = this.platform.height() * 0.2;
       let conmmonStyle =
-        "height:" + demension + "px" + ";" + "width:" + demension + "px" + ";";
+        'height:' + demension + 'px' + ';' + 'width:' + demension + 'px' + ';';
       //
-      this.boxTopLeft = conmmonStyle + "top:" + (demension * -0.62 + "px");
-      this.boxBottomLeft = conmmonStyle + "top:" + (demension * -0.125 + "px");
-      this.boxMidRight = conmmonStyle + "top:" + (demension * 0.2 + "px");
+      this.boxTopLeft = conmmonStyle + 'top:' + (demension * -0.62 + 'px');
+      this.boxBottomLeft = conmmonStyle + 'top:' + (demension * -0.125 + 'px');
+      this.boxMidRight = conmmonStyle + 'top:' + (demension * 0.2 + 'px');
       this.boxBottomRight =
-        conmmonStyle + "marginTop:" + (demension * 0.57 + "px");
+        conmmonStyle + 'marginTop:' + (demension * 0.57 + 'px');
       // console.log("boxBottomRight", this.boxBottomRight, conmmonStyle);
     });
   }
-
-  ngOnInit() {}
 }
